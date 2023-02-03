b66e28ace978a91f739c2edf496cdae817b7d6f0
Dylan <dylan.pilsner@gmail.com>
2023-02-03 19:02:34 -0300

    test

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -6,7 +6,7 @@ export default async function getHandler(
   res: NextApiResponse
 ) {
   const { id, topic } = req.query;
-  if (topic == "merchant_order") {
+  if (topic === "merchant_order") {
     await updateOrderStatus(id as string);
   }
   return res.send({ message: "mercadopago webhook has done its job" });
